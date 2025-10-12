b8176ad3cd522709156159f13af59236672ed144
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-10-12 19:23:59 -0400

    Update compiler-enums.js

--- test/unit/compiler-enums.js
+++ test/unit/compiler-enums.js
@@ -1,20 +1,21 @@
 const test = require('tap').test;
 const {InputOpcode} = require('../../src/compiler/enums');
 
-test('InputOpcode removed deprecated comparison operators', t => {
-    // Verify removed opcodes don't exist
-    t.notOk(Object.values(InputOpcode).includes('op.gtoreq'), 'OP_GTOREQ should not exist');
-    t.notOk(Object.values(InputOpcode).includes('op.ltoreq'), 'OP_LTOREQ should not exist');
+test('InputOpcode has new comparison operators', t => {
+    // Verify new opcodes exist
+    t.ok(Object.values(InputOpcode).includes('op.gtoreq'), 'OP_GTOREQ exists');
+    t.ok(Object.values(InputOpcode).includes('op.ltoreq'), 'OP_LTOREQ exists');
     t.end();
 });
 
-test('InputOpcode has standard comparison operators', t => {
+test('InputOpcode has all comparison operators', t => {
     t.ok(Object.values(InputOpcode).includes('op.equals'), 'OP_EQUALS exists');
     t.ok(Object.values(InputOpcode).includes('op.greater'), 'OP_GREATER exists');
     t.ok(Object.values(InputOpcode).includes('op.less'), 'OP_LESS exists');
+    t.ok(Object.values(InputOpcode).includes('op.gtoreq'), 'OP_GTOREQ exists');
+    t.ok(Object.values(InputOpcode).includes('op.ltoreq'), 'OP_LTOREQ exists');
     t.end();
 });
-
 test('InputOpcode has arithmetic operators', t => {
     t.ok(Object.values(InputOpcode).includes('op.add'), 'OP_ADD exists');
     t.ok(Object.values(InputOpcode).includes('op.subtract'), 'OP_SUBTRACT exists');
